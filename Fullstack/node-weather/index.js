6379daed6fc5763fa080eeec433ff11f41dc3645
Kingsley Chu <kingsleychu0730@hotmail.com>
2020-01-16 14:45:36 -0500

    added openweathermap api code integrated with node js

---
+++ Fullstack/node-weather/index.js
@@ -0,0 +1,15 @@
+let request = require('request');
+
+let apiKey = '4f50bb3a259ec2351894cc2d53ac1719';
+let city = 'toronto';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+request(url, function (err, response, body) {
+  if(err){
+    console.log('error:', error);
+  } else {
+    let weather = JSON.parse(body)
+    let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+    console.log(message);
+  }
+});
